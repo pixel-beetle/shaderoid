2e5b24a6c302ef5c1e119a5faf4fe635db98eb2f
Pixel Beetle <187939376+pixel-beetle@users.noreply.github.com>
2025-09-01 23:00:39 +0800

    Reduce Build Size(2D Nav & Advanced Text）

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1756731371|3063605422';
+const CACHE_VERSION = '1756738778|10470869495';
 /** @type {string} */
 const CACHE_PREFIX = 'shaderoid-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
