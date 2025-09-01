7aa50365c96a2f18879a3118e566ef51bbefc98d
Pixel Beetle <187939376+pixel-beetle@users.noreply.github.com>
2025-09-02 00:07:32 +0800

    update build

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1756738778|10470869495';
+const CACHE_VERSION = '1756742825|14517993630';
 /** @type {string} */
 const CACHE_PREFIX = 'shaderoid-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
